10debf93d475ae60bbb39740ffe7da951222395b
Vivekachooz <vivekachooz@gmail.com>
2020-08-12 09:51:07 +0530

    X00TD: Import thermal-engine.conf
    * From UL-ASUS_X00T-WW-17.2017.2006.429-user
    
    Change-Id: If69cb8a5f8c7ed88a944e12e5f77838fdba39f15

--- device.mk
+++ device.mk
@@ -433,6 +433,10 @@ PRODUCT_BOOT_JARS += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.qti
 
+# Thermal configs
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/thermal-engine.conf:$(TARGET_COPY_OUT_VENDOR)/etc/thermal-engine.conf
+
 # Trust HAL
 PRODUCT_PACKAGES += \
     vendor.lineage.trust@1.0-service
