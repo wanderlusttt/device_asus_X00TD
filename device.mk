8656042c3203ef5a56d37a3678db22e27bb92203
Timi <timi.rautamaki@gmail.com>
2022-02-13 11:01:19 +0530

    X00TD: remove left over qti perf parts
    Change-Id: I2d1e37aa25cf45339cf6e1a6ae482a35e9da0ff8

--- device.mk
+++ device.mk
@@ -360,8 +360,7 @@ PRODUCT_COPY_FILES += \
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power-service.asus_sdm660-libperfmgr \
-    android.hardware.power.stats@1.0-service.mock \
-    vendor.qti.hardware.perf@2.2.vendor
+    android.hardware.power.stats@1.0-service.mock
 
 # Powerhint
 PRODUCT_COPY_FILES += \
