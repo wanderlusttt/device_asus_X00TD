034926c9baa49408c3437887b1fb71587e4f5f1c
blackpanther-007 <sbharathraju3@outlook.com>
2022-08-26 10:25:36 +0530

    X00TD: Build system varitant of libqti_vndfwk_detect and libvndfwk_detect_jni.qti

--- device.mk
+++ device.mk
@@ -405,7 +405,9 @@ PRODUCT_COPY_FILES += \
 # QMI
 PRODUCT_PACKAGES += \
     libjson \
+    libqti_vndfwk_detect \
     libqti_vndfwk_detect.vendor \
+    libvndfwk_detect_jni.qti \
     libvndfwk_detect_jni.qti.vendor
 
 PRODUCT_COPY_FILES += \
